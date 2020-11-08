7d9fc05e970593bf9d483e7db87dc5afc474a1d3
phaesol <parkhaesol0227@likelion.org>
2020-11-08 16:06:07 +0900

    정리한거 다시..카테고리 리그룹화 필요

--- ksproject/main/views.py
+++ ksproject/main/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render,redirect
 from board.models import Category,Mini_Category
 
 
-def index(request,category_id):
+def index(request):
     context = dict()
 
     categories = Category.objects.all()
     context['categories'] = categories
 
-    mini_category_group = Category.objects.get(id=category_id)
-    mini_category = Mini_Category.objects.all()
+
+    mini_categories = Mini_Category.objects.all()
     context['mini_categories'] = mini_categories
     
     return render(request,'index.html', context)
